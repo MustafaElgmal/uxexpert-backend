633d8b30b85dd9f8fc195bbea015877be0b7feaa
Mustafa Elgmal <mostafaelgmal36@gmail.com>
2023-03-14 14:51:23 +0200

    Add notfound

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,10 +1,10 @@
 import natural from 'natural'
 export const classifySentence = (sentence:string) => {
     const classifier = new natural.BayesClassifier();
+    classifier.addDocument("XLTp7S6rne", "NotFound");
     classifier.addDocument("medical hospital doctor nurse patient", "medical");
-    classifier.addDocument("pizza burger restaurant  food", "food");
+    classifier.addDocument("pizza burger restaurant food", "food");
     classifier.addDocument("teacher student teaching learning school", "elearning");
-    classifier.addDocument("", "notFound");
     // Train
     classifier.train();
     // Apply/Predict
