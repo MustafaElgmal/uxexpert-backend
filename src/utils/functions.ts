5506fed96f457d3ca17f555bffc848102d156cf5
Mustafa Elgmal <mostafaelgmal36@gmail.com>
2023-03-14 14:35:28 +0200

    Add not found

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -3,8 +3,8 @@ export const classifySentence = (sentence:string) => {
     const classifier = new natural.BayesClassifier();
     classifier.addDocument("medical hospital doctor nurse patient", "medical");
     classifier.addDocument("pizza burger restaurant  food", "food");
-    classifier.addDocument("buying selling offers  discount", "ecommerce");
     classifier.addDocument("teacher student teaching learning school", "elearning");
+    classifier.addDocument("", "notFound");
     // Train
     classifier.train();
     // Apply/Predict
